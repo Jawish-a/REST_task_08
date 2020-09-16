941f608e952627998c8ea73786287d61a26a5f63
jawish-a@outlook.com <jawish-a@outlook.com>
2020-09-16 16:51:45 +0300

    done

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from django.http import JsonResponse
 
 from .models import Flight, Booking, Profile
-
+import datetime
 
 class FlightSerializer(serializers.ModelSerializer):
 	class Meta:
@@ -11,15 +12,24 @@ class FlightSerializer(serializers.ModelSerializer):
 
 
 class BookingSerializer(serializers.ModelSerializer):
+	flight = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='destination'
+	)
 	class Meta:
 		model = Booking
 		fields = ['flight', 'date', 'id']
 
 
 class BookingDetailsSerializer(serializers.ModelSerializer):
+	total = serializers.SerializerMethodField()
+	flight = FlightSerializer()
 	class Meta:
 		model = Booking
-		fields = ['flight', 'date', 'passengers', 'id']
+		fields = ['flight', 'date', 'passengers', 'id', 'total']
+	
+	def get_total(self, obj):
+		return obj.passengers * obj.flight.price
 
 
 class AdminUpdateBookingSerializer(serializers.ModelSerializer):
@@ -52,7 +62,29 @@ class RegisterSerializer(serializers.ModelSerializer):
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+	user = serializers.SerializerMethodField()
+	past_bookings = serializers.SerializerMethodField()
+
+	tier = serializers.SerializerMethodField()
 	class Meta:
 		model = Profile
-		fields = ['user', 'miles']
+		fields = ['user', 'miles', 'past_bookings', 'tier']
 
+	def get_user(self, obj):
+		return {"first_name": obj.user.first_name,"last_name": obj.user.last_name,}
+	
+	def get_past_bookings(self, obj):
+		past_bookings = Booking.objects.filter(user=obj.user, date__lt=datetime.date.today())
+		return BookingSerializer(past_bookings, many=True).data
+
+	def get_tier(self, obj):
+		if 0 <= obj.miles <= 9999:
+			return 'Blue'
+		elif 10000 <= obj.miles <= 59999:
+			return 'Silver'
+		elif 60000 <= obj.miles <= 99999:
+			return 'Gold'
+		elif obj.miles <= 100000:
+			return 'Platinum'
+		else:
+			return 'no tier'
